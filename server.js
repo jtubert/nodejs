c77e9f5f1168e4d07209a174766586f3fc1e6a81
John Tubert <jtubert@hotmail.com>
2011-08-28 01:33:37 -0300

    tets

--- server.js
+++ server.js
@@ -1,4 +1,33 @@
-require("http").createServer(function (req,res) {
-  res.writeHead(200,{})
-  res.end("Hello, world!")
-}).listen(80)
+var app = require('http').createServer(handler), 
+	io = require('socket.io').listen(app), 
+	fs = require('fs')
+
+app.listen(1337, "127.0.0.1");
+
+
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/client.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading client.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.sockets.on('connection', function (socket) {
+  
+  socket.on('news', function (data) {
+    socket.broadcast.emit('news', { hello: data });
+  });
+});
+
+
+//to install
+//npm install socket.io
+//http://socket.io/#how-to-use
+
