3f18951b54a14142dc0f18fe0ae01d4fb6f470b2
John Tubert <jtubert@hotmail.com>
2011-08-29 09:50:54 -0300

    console sessionid

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ function handler (req, res) {
 
 io.sockets.on('connection', function (socket) {
 	
-  console.log("-----------------length: "+io.sockets.clients().length+" / ");
+  console.log("-----------------length: "+io.sockets.clients().length+" / "+socket.sessionid);
   
   socket.on('move', function (data) {
     socket.broadcast.emit('move', { draw: data });
