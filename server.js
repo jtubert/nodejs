b237f6626110eb606741ace2ab4aab3e1d6aadd3
John Tubert <jtubert@hotmail.com>
2011-08-29 17:07:08 -0300

    test

--- server.js
+++ server.js
@@ -29,25 +29,24 @@ io.sockets.on('connection', function (socket) {
 		//console.log("-----------------length: "+io.sockets.clients().length+" / "+socket.id);
 	}
 	
-	socket.on('nickname', function (name) {
+	socket.on('setName', function (name) {
 	    socket.set('nickname', name, function () {
 	      //socket.emit('ready');
 	    });
 	});
 	
-	var nickname;
-	
-	socket.get('nickname', function (err, name) {
-		nickname = name;
-	});
-	
   
 	socket.on('move', function (data) {
-	  socket.broadcast.emit('move', { draw: data,nickname:nickname });
+		socket.get('nickname', function (err, name) {
+			socket.broadcast.emit('move', { draw: data,nickname:name });
+		});	  
 	});
 
 	socket.on('down', function (data) {
-	  socket.broadcast.emit('down', { draw: data,nickname:nickname });
+		socket.get('nickname', function (err, name) {
+			socket.broadcast.emit('down', { draw: data,nickname:nickname });
+		});
+	  
 	});
 
 	socket.on('erase', function (data) {
