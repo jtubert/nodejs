02e0fae179d5617bda55e2ef0c14b0ba86cdae9d
John Tubert <jtubert@hotmail.com>
2012-04-16 17:11:21 -0400

    .

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var app = require('express').createServer(),
 app.use(gzippo.staticGzip(__dirname + '/static'));
 
 var users = [];
-var activeClients = 0;
+//var activeClients = 0;
 
 if(process.argv[2] == "local" || process.argv[2] == "localhost"){
 	//FOR LOCALHOST
@@ -24,31 +24,32 @@ if(process.argv[2] == "local" || process.argv[2] == "localhost"){
 }
 
 //console.log("*****************"+process.argv[2]);
-
+/*
 function clientDisconnect(client){
   activeClients -=1;
   client.broadcast({clients:activeClients});
 }
+*/
 
 io.sockets.on('connection', function (socket) {	
 	//console.log("connections: "+socket.namespace.manager.server.connections+" / "+io.sockets.clients().length);
 	//console.log("-----------------length: "+io.sockets.clients().length+" / "+socket.id);
-	
+	/*
 	activeClients +=1;
 	socket.broadcast({clients:activeClients});
 	socket.on('disconnect', function(){clientDisconnect(socket)});
-	
+	*/
 	
 	
 	socket.on('connect',function(data){
 		socket.broadcast.emit('connect', {connections: io.sockets.clients().length,connections2: socket.namespace.manager.server.connections,connections3: users.length});
 		socket.emit('connect2', {connections: io.sockets.clients().length,connections2: socket.namespace.manager.server.connections,connections3: users.length});
 		
-	});
-	
+	});	
 	
     socket.on('disconnect', function () {
-        socket.broadcast.emit('disconnect', {connections: users.length});
+        socket.broadcast.emit('connect', {nickname:name,connections: io.sockets.clients().length});
+		socket.emit('connect2', {nickname:name,connections: io.sockets.clients().length});
     });
 	
 	socket.on('setName', function (name) {		
